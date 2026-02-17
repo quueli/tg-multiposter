13ddae768cdf1994a4a180879d9f43d6a66c8f4a
quueli <rampadian@mail.ru>
2026-02-17 19:10:16 +0300

    system admins can use any instance

--- template/config.py
+++ template/config.py
@@ -4,11 +4,11 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-# set per instance by the manager when it provisions this copy
+# written per instance by the manager
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 OWNER_ID = int(os.getenv("OWNER_ID", "0"))
 
-# manager bot users are pointed to when they want their own instance
+ADMINS = [int(x) for x in os.getenv("ADMIN_IDS", "123456789").split(",") if x.strip()]
 MAIN_BOT = os.getenv("MANAGER_BOT_USERNAME", "@your_manager_bot")
 
 DATA_FILE = "data.json"
